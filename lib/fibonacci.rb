525d07f784f81a602f76886e68624a5817719fb5
Sherry He <xsherryhe@gmail.com>
2022-04-18 13:00:31 -0400

    Completed merge_sort.rb

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -6,7 +6,7 @@ end
 
 def fibs_rec(n)
   return [] if n.zero?
-  sequence = fibs_rec(n - 1)
 
+  sequence = fibs_rec(n - 1)
   sequence << (sequence[-1] || -1) + (sequence[-2] || 1)
 end
---
+++ lib/merge_sort.rb
@@ -0,0 +1,13 @@
+def merge_sort(arr)
+  return arr if arr.size == 1
+
+  left, right = arr.each_slice((arr.size / 2.0).ceil)
+                   .map { |half| merge_sort(half) }
+  merge(left, right)
+end
+
+def merge(left, right)
+  sorted_arr = []
+  sorted_arr << (left.first < right.first ? left.shift : right.shift) until left.empty? || right.empty?
+  sorted_arr + left + right
+end
